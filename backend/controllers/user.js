62e997c3f83838136c0b6610e146212d6e250ffd
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-03-04 17:29:04 +0000

    -Changed Secret Token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,20 +4,18 @@ const bcrypt = require('bcrypt')
 // Secret or Private Key (Token) generator for verification purpose
 const jwt = require('jsonwebtoken')
 
-// Masking Sensitive User Input (Email)
-const MaskData = require('maskdata')
+// Token
+const crypto = require('crypto')
 
 // User models
 const User = require('../models/user')
 
 // SignUp function
 exports.signup = (req, res, next) => {
-  const email = req.body.email
-  const maskedEmail = MaskData.maskEmail2(email)
   bcrypt.hash(req.body.password, 10).then(
     (hash) => {
       const user = new User({
-        email: maskedEmail,
+        email: req.body.email,
         password: hash
       })
       user.save().then(
@@ -45,9 +43,7 @@ exports.signup = (req, res, next) => {
 
 // LogIn function
 exports.login = (req, res, next) => {
-  const email = req.body.email
-  const maskedEmail = MaskData.maskEmail2(email)
-  User.findOne({ email: maskedEmail }).then(
+  User.findOne({ email: req.body.email }).then(
     (user) => {
       if (!user) {
         return res.status(401).json({
@@ -61,7 +57,8 @@ exports.login = (req, res, next) => {
               error: new Error('Incorrect password!')
             })
           }
-          const token = jwt.sign({ userId: user._id }, 'RANDOM_SECRET_TOKEN', { expiresIn: '24h' })
+          const secret = crypto.randomBytes(16).toString('hex')
+          const token = jwt.sign({ userId: user._id }, secret, { expiresIn: '24h' })
           res.status(200).json({
             userId: user._id,
             token: token
